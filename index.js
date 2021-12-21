8e32d4d2e32d740151ba4106d23c83728b0ca168
Irina-Berizinska <80217790+Irina-Berizinska@users.noreply.github.com>
2021-12-21 22:19:47 +0200

    1

--- index.js
+++ index.js
@@ -31,3 +31,42 @@ if (process.env.NOW_URL || process.env.HEROKU_URL) {
 } else {
     logger.debug('Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.');
 }
+function say(response, message) {
+    response.send(new TextMessage(message));
+}
+bot.onSubscribe(response => {
+    say(response, `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me if a web site is down for everyone or just you. Just send me a name of a website and I'll do the rest!`);
+});
+
+function checkUrlAvailability(botResponse, urlToCheck) {
+
+    if (urlToCheck === '') {
+        say(botResponse, 'I need a URL to check');
+        return;
+    }
+
+    say(botResponse, 'One second...Let me check!');
+
+    var url = urlToCheck.replace(/^http:\/\//, '');
+    request('http://isup.me/' + url, function(error, requestResponse, body) {
+        if (error || requestResponse.statusCode !== 200) {
+            say(botResponse, 'Something is wrong with isup.me.');
+            return;
+        }
+
+        if (!error && requestResponse.statusCode === 200) {
+            if (body.search('is up') !== -1) {
+                say(botResponse, 'Hooray! ' + urlToCheck + '. looks good to me.');
+            } else if (body.search('Huh') !== -1) {
+                say(botResponse, 'Hmmmmm ' + urlToCheck + '. does not look like a website to me. Typo? please follow the format `test.com`');
+            } else if (body.search('down from here') !== -1) {
+                say(botResponse, 'Oh no! ' + urlToCheck + '. is broken.');
+            } else {
+                say(botResponse, 'Snap...Something is wrong with isup.me.');
+            }
+        }
+    })
+}
+bot.onTextMessage(/./, (message, response) => {
+    checkUrlAvailability(response, message.text);
+})
